e573d0d4f1311ccbc7d7cc9b4e640ee29701b31a
LEDvalhol <valera.holera91@gmail.com>
2023-03-29 21:54:54 +0300

    describe local variables, global variables

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-# PyStudy
-
-use variables
-use incapsulate
-use functions, create days_to_units
-use functions, use parameters
-use functions, scope
+describe local and global scope
--- days_calculator/days_calc.py
+++ days_calculator/days_calc.py
@@ -1,11 +1,21 @@
+""" THIS IS THE GLOBAL SCOPE """
 calculations_to_unit = 20 * 24 * 60 * 60
 name_of_units = "seconds"
 
+""" THIS IS THE LOCAL SCOPE """
+
 
 def days_to_units(num_of_days, custom_message):
-
     print(f"{num_of_days} days are {num_of_days * calculations_to_unit} {name_of_units}")
     print(custom_message)
 
 
+def scope_check(num_of_days):
+    my_var = "a variable inside function"
+    print(name_of_units)
+    print(num_of_days)
+    print(my_var)
+
+
 days_to_units(35, "Awesome!")
+scope_check(20)
