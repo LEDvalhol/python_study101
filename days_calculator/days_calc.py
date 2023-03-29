b58bee85cdf662e24625a696aee03d749486d870
LEDvalhol <valera.holera91@gmail.com>
2023-03-29 18:51:03 +0300

    use variables

---
+++ days_calculator/days_calc.py
@@ -0,0 +1,5 @@
+calculations_to_unit = 20 * 24 * 60 * 60
+name_of_units = "seconds"
+
+print(f"20 days are {20 * calculations_to_unit} {name_of_units}")
+print(f"20 days are {20 * calculations_to_unit} {name_of_units}")
