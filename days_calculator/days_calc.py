89b3ee55e18249b23f4a3cd6f15c572a589a823d
LEDvalhol <valera.holera91@gmail.com>
2023-03-30 22:13:28 +0300

    describe bool

--- days_calculator/days_calc.py
+++ days_calculator/days_calc.py
@@ -3,11 +3,21 @@ name_of_units = "seconds"
 
 
 def days_to_units(num_of_days):
-    return f"{num_of_days} days are {num_of_days * calculations_to_units} {name_of_units}"
+    if num_of_days > 0:
+        return f"{num_of_days} days are {num_of_days * calculations_to_units} {name_of_units}"
+    elif num_of_days == 0:
+        return "You entered a zero, please enter a valid positive number"
+    else:
+        return "Negative number, no conversion for you"
 
 
 user_input = input("Hey user, enter number of days and I will convert it for you!\n")
-user_input_number = int(user_input)
+if user_input.isdigit():
+    user_input_number = int(user_input)
+    calculated_value = days_to_units(user_input_number)
+    print(calculated_value)
+else:
+    print("Your input is not a number. Please, provide a valid entry.")
 
-calculated_value = days_to_units(user_input_number)
-print(calculated_value)
+
+
