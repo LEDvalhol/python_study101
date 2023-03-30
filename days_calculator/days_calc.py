132b40e62c22ae901dd4560c66b1b57f607fef3f
LEDvalhol <valera.holera91@gmail.com>
2023-03-30 14:36:58 +0300

    describe scope, User Imput

--- days_calculator/days_calc.py
+++ days_calculator/days_calc.py
@@ -3,10 +3,11 @@ name_of_units = "seconds"
 
 
 def days_to_units(num_of_days):
-    print(f"{num_of_days} days are {num_of_days * calculations_to_units} {name_of_units}")
+    return f"{num_of_days} days are {num_of_days * calculations_to_units} {name_of_units}"
 
 
-days_to_units(20)
-days_to_units(30)
+user_input = input("Hey user, enter number of days and I will convert it for you!\n")
+user_input_number = int(user_input)
 
-
+calculated_value = days_to_units(user_input_number)
+print(calculated_value)
