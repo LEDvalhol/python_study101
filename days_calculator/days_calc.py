fdb1fe0cee746e5035e79b55c6e901d1bec36852
LEDvalhol <valera.holera91@gmail.com>
2023-03-30 12:55:10 +0300

    describe local variables, global variables, add info to README

--- days_calculator/days_calc.py
+++ days_calculator/days_calc.py
@@ -1,21 +1,12 @@
-""" THIS IS THE GLOBAL SCOPE """
-calculations_to_unit = 20 * 24 * 60 * 60
+calculations_to_units = 20 * 24 * 60 * 60
 name_of_units = "seconds"
 
-""" THIS IS THE LOCAL SCOPE """
+
+def days_to_units(num_of_days):
+    print(f"{num_of_days} days are {num_of_days * calculations_to_units} {name_of_units}")
 
 
-def days_to_units(num_of_days, custom_message):
-    print(f"{num_of_days} days are {num_of_days * calculations_to_unit} {name_of_units}")
-    print(custom_message)
+days_to_units(20)
+days_to_units(30)
 
 
-def scope_check(num_of_days):
-    my_var = "a variable inside function"
-    print(name_of_units)
-    print(num_of_days)
-    print(my_var)
-
-
-days_to_units(35, "Awesome!")
-scope_check(20)
